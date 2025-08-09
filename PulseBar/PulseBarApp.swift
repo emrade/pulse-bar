15eb04c80a83c05ac39853ba61f9c21ac5f78d2e
Emmanuel Fache <emrade95@gmail.com>
2025-08-09 07:04:09 +0100

    Initial Commit

---
+++ PulseBar/PulseBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseBarApp.swift
+//  PulseBar
+//
+//  Created by Emmanuel Fache on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PulseBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulseBarTests/PulseBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  PulseBarTests.swift
+//  PulseBarTests
+//
+//  Created by Emmanuel Fache on 09/08/2025.
+//
+
+import Testing
+@testable import PulseBar
+
+struct PulseBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
