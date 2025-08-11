d78bcfd8c3936c102facdc03e2bd1273d4ba45a4
Emmanuel Fache <emrade95@gmail.com>
2025-08-11 12:02:26 +0100

    chore: sizing update

--- PulseBar/Utils/WindowSizing.swift
+++ PulseBar/Utils/WindowSizing.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 enum WindowSizing {
     static let mainWindowWidth: CGFloat = 400
-    static let mainWindowHeight: CGFloat = 550
+    static let mainWindowHeight: CGFloat = 600
     
     static let mainWindowSize = CGSize(width: mainWindowWidth, height: mainWindowHeight)
     
